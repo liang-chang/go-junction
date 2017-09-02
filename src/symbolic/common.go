095f2d40ae33e1f9c7c8630cb6e643ffc779e788
changsheng <changsheng@wacai.com>
2017-09-02 21:09:57 +0800

    代码提交

--- src/symbolic/common.go
+++ src/symbolic/common.go
@@ -5,48 +5,48 @@ package symbolic
 //https://github.com/golang/go/blob/master/src/os/os_windows_test.go
 
 import (
-	"syscall"
-	"unsafe"
 	"strings"
+	"syscall"
 	"unicode/utf16"
+	"unsafe"
 )
 
 func IsReparsePoint(path string) (ret bool, err error) {
-	attrs, err := syscall.GetFileAttributes(syscall.StringToUTF16Ptr(path));
+	attrs, err := syscall.GetFileAttributes(syscall.StringToUTF16Ptr(path))
 	if err != nil {
 		return false, err
 	}
-	return attrs & syscall.FILE_ATTRIBUTE_DIRECTORY > 0, nil
+	return attrs&syscall.FILE_ATTRIBUTE_DIRECTORY > 0, nil
 }
 
 /*
 reparseType -> syscall.IO_REPARSE_TAG_SYMLINK 符号链接
 	       IO_REPARSE_TAG_MOUNT_POINT     junction
- */
+*/
 func getReparseTarget(handle syscall.Handle) (target string, reparseType int, err error) {
 
-	var bytesReturned uint32;
+	var bytesReturned uint32
 
-	var outBuffer MountPointReparseBuffer;
+	var outBuffer MountPointReparseBuffer
 
 	//junction point => IO_REPARSE_TAG_MOUNT_POINT (0xA0000003).
 	//symbolink point => SYMBOLIC_LINK_FLAG_DIRECTORY  (0xA000000C)
 	err = syscall.DeviceIoControl(handle, syscall.FSCTL_GET_REPARSE_POINT,
-		nil, 0, (*byte)(unsafe.Pointer(&outBuffer)), uint32(unsafe.Sizeof(outBuffer)), &bytesReturned, nil);
+		nil, 0, (*byte)(unsafe.Pointer(&outBuffer)), uint32(unsafe.Sizeof(outBuffer)), &bytesReturned, nil)
 
 	if err == nil {
 
 		if outBuffer.ReparseTag == syscall.IO_REPARSE_TAG_SYMLINK {
 
-			var symbolicLink *SymbolicLinkReparseBuffer;
+			var symbolicLink *SymbolicLinkReparseBuffer
 			symbolicLink = (*SymbolicLinkReparseBuffer)(unsafe.Pointer(&outBuffer))
 
 			target = symbolicLink.SubstituteName()
 		} else if outBuffer.ReparseTag == IO_REPARSE_TAG_MOUNT_POINT {
-			target = outBuffer.SubstituteName();
+			target = outBuffer.SubstituteName()
 		}
 		reparseType = int(outBuffer.ReparseTag)
-		if (strings.HasPrefix(target, NonInterpretedPathPrefix)) {
+		if strings.HasPrefix(target, NonInterpretedPathPrefix) {
 			target = target[len(NonInterpretedPathPrefix):]
 		}
 	} else {
@@ -59,10 +59,10 @@ func openReparsePoint(reparsePoint string, accessMode uint32) (handle syscall.Ha
 	handle, err = syscall.CreateFile(
 		syscall.StringToUTF16Ptr(reparsePoint),
 		accessMode,
-		syscall.FILE_SHARE_READ | syscall.FILE_SHARE_WRITE | syscall.FILE_SHARE_DELETE,
+		syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE|syscall.FILE_SHARE_DELETE,
 		nil,
 		syscall.OPEN_EXISTING,
-		syscall.FILE_FLAG_BACKUP_SEMANTICS | syscall.FILE_FLAG_OPEN_REPARSE_POINT,
+		syscall.FILE_FLAG_BACKUP_SEMANTICS|syscall.FILE_FLAG_OPEN_REPARSE_POINT,
 		0)
 	return
 }
@@ -70,23 +70,23 @@ func openReparsePoint(reparsePoint string, accessMode uint32) (handle syscall.Ha
 func (r *SymbolicLinkReparseBuffer) PrintName() string {
 	offset := r.PrintNameOffset / UNINT16_SIZE
 	length := r.PrintNameLength / UNINT16_SIZE
-	return string(utf16.Decode(r.PathBuffer[offset:offset + length]))
+	return string(utf16.Decode(r.PathBuffer[offset : offset+length]))
 }
 
 func (r *SymbolicLinkReparseBuffer) SubstituteName() string {
 	offset := r.SubstituteNameOffset / UNINT16_SIZE
 	length := r.SubstituteNameLength / UNINT16_SIZE
-	return string(utf16.Decode(r.PathBuffer[offset:offset + length]))
+	return string(utf16.Decode(r.PathBuffer[offset : offset+length]))
 }
 
 func (r *MountPointReparseBuffer) PrintName() string {
 	offset := r.PrintNameOffset / UNINT16_SIZE
 	length := r.PrintNameLength / UNINT16_SIZE
-	return string(utf16.Decode(r.PathBuffer[offset:offset + length]))
+	return string(utf16.Decode(r.PathBuffer[offset : offset+length]))
 }
 
 func (r *MountPointReparseBuffer) SubstituteName() string {
 	offset := r.SubstituteNameOffset / UNINT16_SIZE
 	length := r.SubstituteNameLength / UNINT16_SIZE
-	return string(utf16.Decode(r.PathBuffer[offset:offset + length]))
+	return string(utf16.Decode(r.PathBuffer[offset : offset+length]))
 }
--- src/util/init.go
+++ src/util/init.go
@@ -12,5 +12,5 @@ func init() {
 }
 
 func Log(i ...interface{}) {
-	Log(i)
+	logger.Print(i)
 }
